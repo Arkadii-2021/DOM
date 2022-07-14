cb35d87c210712603f1552311fb659b02840571f
Arkadii-2021 <88792842+Arkadii-2021@users.noreply.github.com>
2022-07-14 10:41:06 +0700

    rename name function

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -4,14 +4,14 @@ const controlFontSize = document.querySelectorAll('.font-size');
 function changeFontSize() {
 	Array.from(controlFontSize).forEach(fontSize => {
 		fontSize.addEventListener('click', (event) => {
-			_fs();
+			_resetClasses();
 			event.target.classList.add('font-size_active');
 			bookContent.classList.add(`font-size_${fontSize.dataset.size}`);
 			event.preventDefault();
 		})
 })};
 
-function _fs() {
+function _resetClasses() {
 	Array.from(controlFontSize).forEach(activeClass => {
 		activeClass.classList.remove('font-size_active');
 	});
