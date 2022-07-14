e2c26c4d54bdf8bb2dbddb3cd519e8b3ec83c43c
Arkadii-2021 <88792842+Arkadii-2021@users.noreply.github.com>
2022-07-14 10:37:01 +0700

    Update task.js

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -9,10 +9,10 @@ function rotator() {
 	setInterval (() => {
 		for(let i = 0; i < rotatorCase.length; i++){
 			if(rotatorCase[i].classList.contains('rotator__case_active')){
-					rotatorCase[i].classList.remove('rotator__case_active');
+				rotatorCase[i].classList.remove('rotator__case_active');
 					(i+1 < rotatorCase.length) ? rotatorCase[++i].classList.add('rotator__case_active') : rotatorCase[0].classList.add('rotator__case_active');
-				}
 			}
+		}
 }, 1000);
 };
 
